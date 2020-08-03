913e6ce7792acdd8d1a9b958effed2b0566e0aa1
Alexandro Antonio Blini Maceiras <alexandro@maceiras.com.br>
2020-08-03 05:16:36 -0300

    Add files via upload

--- Startup.cs
+++ Startup.cs
@@ -1,15 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using AlbertEinstein.Models;
+using AlbertEinstein.Services;
+using Consultas.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace AlbertEinstein
 {
@@ -25,7 +23,34 @@ namespace AlbertEinstein
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AlbertEinsteinContext>(options =>
+             options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            //Registrando a DI (Injeção de Dependência)
+             services.AddTransient<IConsultaItemService, ConsultaItemService>(); 
+             services.AddTransient<IOutrosItemService, OutrosItemService>();
+
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                OpenApiContact Contato = new OpenApiContact()
+                {
+                    Email = "alexandro@maceiras.com.br",
+                    Name = "Click Here"
+                };
+
+                OpenApiLicense Licenca = new OpenApiLicense();
+                Licenca.Name = "Copyright(C)";
+
+                c.SwaggerDoc("v1", new OpenApiInfo 
+                { 
+                    Title = "AlbertEinstein", 
+                    Version = "Versão 1.0.0", 
+                    Description = "Aplicação de Teste do Albert Einstein",
+                    Contact = Contato,
+                    License = Licenca
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,6 +64,17 @@ namespace AlbertEinstein
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "WEB.API Versão 1.0.0");
+                c.RoutePrefix = string.Empty;
+            });
 
             app.UseAuthorization();
 
